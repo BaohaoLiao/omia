53fa228870228ecd9144cd1c2fc489490e3d9b47
baohaoliao <liaobaohao@gmail.com>
2025-02-02 20:01:13 +0100

    main

--- main.py
+++ main.py
@@ -174,6 +174,7 @@ def setup(args):
             gpu_memory_utilization=0.95,
             seed=args.seed,
             num_speculative_tokens=args.num_speculative_tokens,
+            disable_log_stats=False,
             #ngram_prompt_lookup_max=4,
         )
     tokenizer = llm.get_tokenizer()
