832f99baf8b693f7284b6861975d5185ea815f50
baohaoliao <liaobaohao@gmail.com>
2025-02-03 12:53:27 +0100

    main

--- main.py
+++ main.py
@@ -62,13 +62,10 @@ def select_answer(answers, lengths):
     # Find the maximum frequency
     max_freq = max(freq_dict.values())
     
-    print(answers, lengths)
-    print(freq_dict, valid_indices)
     # Get all answers with maximum frequency
     most_common = [ans for ans, freq in freq_dict.items() if freq == max_freq]
     if len(most_common) == 1:
-        indices = [i for i in valid_indices if answers[i] == most_common[0]]
-        print(most_common, indices)
+        indices = [i for i in valid_indices if answers[i] == str(most_common[0])]
         avg_length = sum([lengths[i] for i in indices]) / len(indices)
         return most_common[0], avg_length
     
