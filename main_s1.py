7f79865f53bb01dce95e8a995b457cec971eb860
baohaoliao <liaobaohao@gmail.com>
2025-02-12 21:54:22 +0100

    data

--- main_s1.py
+++ main_s1.py
@@ -271,7 +271,7 @@ def setup(args):
 
 
 def main(llm, tokenizer, args):
-    df = pd.read_csv(os.path.join(args.data_dir, args.data_name, "reference.csv"))
+    df = pd.read_csv(os.path.join(args.data_dir, args.data_name + ".csv"))
     if args.num_test_sample != -1:
         assert args.num_test_sample <= len(df)
         df = df[:args.num_test_sample]
