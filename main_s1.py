225939b663d281c23825388d566de956e51b7ff6
baohaoliao <liaobaohao@gmail.com>
2025-02-13 15:38:48 +0100

    main_s1

--- main_s1.py
+++ main_s1.py
@@ -218,35 +218,35 @@ def batch_message_generate(llm, tokenizer, list_of_messages, args):
     return list_of_lengths_and_messages
 
 SYSTEMP_PROMPTS = [
-    "You are a the most powerful math expert. Please solve the problems with deep resoning. You are careful and always recheck your conduction. You will never give answer directly until you have enough confidence. You should think step-by-step. Return final answer within \\boxed{}, after taking modulo 1000.",
-    "Please reason step by step, and put your final answer within \\boxed{}.",
+    {
+        "system": "You are a the most powerful math expert. Please solve the problems with deep resoning. You are careful and always recheck your conduction. You will never give answer directly until you have enough confidence. You should think step-by-step. Return final answer within \\boxed{}, after taking modulo 1000.",
+        "suffix": "",
+    },
+    {
+        "system": "Please reason step by step, and put your final answer within \\boxed{}.",
+        "suffix": "",
+    },
+    {
+        "system": "You are a helpful and harmless math assistant. You should think step-by-step and you are good at reverse thinking to recheck your answer and fix all possible mistakes. After you get your final answer, take modulo 1000, and return the final answer within \\boxed{}.",
+        "suffix": "",
+    },
+    {
+        "system": "Please carefully read the problem statement first to ensure you fully understand its meaning and key points. Then, solve the problem correctly and completely through deep reasoning. Finally, return the result modulo 1000 and enclose it in \\boxed{} like \"Atfer take the result modulo 1000, final anwer is \\boxed{180}.",
+        "suffix": "",
+    },
 ]
+
 
 def create_starter_messages(question, index, args):
     options = []
     for _ in range(args.max_num_seqs):
         options.append(
             [
-                {"role": "system", "content": SYSTEMP_PROMPTS[args.prompt_type]},
-                {"role": "user", "content": question},
+                {"role": "system", "content": SYSTEMP_PROMPTS[args.prompt_type]["system"]},
+                {"role": "user", "content": question + SYSTEMP_PROMPTS[args.prompt_type]["suffix"]},
             ]
         )
-    """
-    for _ in range(2):    
-        options.append(
-            [
-                {"role": "system", "content": "You are a helpful and harmless math assistant. You should think step-by-step and you are good at reverse thinking to recheck your answer and fix all possible mistakes. After you get your final answer, take modulo 1000, and return the final answer within \\boxed{}."},
-                {"role": "user", "content": question},
-            ],
-        )
-    options.append(
-        [
-            {"role": "system", "content": "Please carefully read the problem statement first to ensure you fully understand its meaning and key points. Then, solve the problem correctly and completely through deep reasoning. Finally, return the result modulo 1000 and enclose it in \\boxed{} like \"Atfer take the result modulo 1000, final anwer is \\boxed{180}."},
-            {"role": "user", "content": question},
-        ],
-    )
-    """
-    return options[index%len(options)]
+    return options
 
 
 def predict_for_question(llm, tokenizer, question, args):
